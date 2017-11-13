7a0310e90b94c26734008e6bb26eef0476063a23
roxy71 <roxana71janet@gmail.com>
2017-11-13 19:09:08 -0300

    Conexion de Java con BD MySQL

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -4,18 +4,26 @@
  * and open the template in the editor.
  */
 package conexion;
+import Conexion.ConexionBD;
+import javax.swing.JFrame;
 
 /**
  *
  * @author LAB03
  */
-public class Conexion {
-
+public class Conexion 
+{
+ 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+          Interfaz1 w = new Interfaz1();
+          w.setVisible(true);
+          w.setBounds(0, 0, 400, 400);
+          w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          
     }
     
 }
